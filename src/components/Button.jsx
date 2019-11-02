6f0162759988d34f985b63ea2608465fac24670e
Eduardo Lemos <eddu1930@gmail.com>
2019-11-02 17:02:28 -0400

    Buttons, style, and functionality.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./Button.css";
 
 const isOperator = val => {
+  // If it's not NotaNumber, ., =, we do nothing
+  // Else add class "operator"
   return !isNaN(val) || val === "." || val === "=";
 };
 
-const Button = props => (
+export const Button = props => (
   <div
     className={`button-wrapper ${
       isOperator(props.children) ? null : "operator"
     }`}
+    onClick={() => props.handleClick(props.children)}
   >
     {props.children}
   </div>
 );
-
-export default Button;
