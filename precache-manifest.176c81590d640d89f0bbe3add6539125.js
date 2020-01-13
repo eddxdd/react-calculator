b67cb832168dc74638cfcd02d08a6a17c53fe060
Eduardo Lemos <eddu1930@gmail.com>
2020-01-12 20:52:49 -0500

    Updates

---
+++ precache-manifest.176c81590d640d89f0bbe3add6539125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d187bb69daf29959ccbc206ee209ce",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "533d906d012c024c3b75",
+    "url": "/react-calculator/static/css/main.4848112e.chunk.css"
+  },
+  {
+    "revision": "bfc03a1bf2e9844ba91d",
+    "url": "/react-calculator/static/js/2.fa262806.chunk.js"
+  },
+  {
+    "revision": "533d906d012c024c3b75",
+    "url": "/react-calculator/static/js/main.27e52d6f.chunk.js"
+  },
+  {
+    "revision": "13f74d5b07f8a126925e",
+    "url": "/react-calculator/static/js/runtime-main.7b818f79.js"
+  }
+]);
